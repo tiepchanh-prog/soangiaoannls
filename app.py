3079cf8d0315e82b659b6045e3c2e57cb1f66eab
tiepchanh-prog <tiepchanh@gmail.com>
2025-12-30 08:59:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Hoàng Đức Tiệp - Trường PTDTBT Tiểu học Thông Nguyên - ĐT: 0329 863 168</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Hoàng Đức Tiệp - Trường PTDTBT Tiểu học Thông Nguyên - ĐT: 0329 863 168</div>", unsafe_allow_html=True)
